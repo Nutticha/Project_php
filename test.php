e8dbd9dd40a4740603e5396b47ed5e91b5c4aef1
Nutticha <nutticha30234@gmail.com>
2021-02-11 22:39:12 +0700

    up

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
   
   $con = mysqli_connect(" https://node10789-am-project.app.ruk-com.cloud" , "root" , "PNMzyc57892" , "applestore");
-  if($con) echo "เชื่อม9jvสำเร็จ";
+  if($con) echo "เชื่อมต่อสำเร็จ";
